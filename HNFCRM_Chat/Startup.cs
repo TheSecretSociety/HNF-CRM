617eafeaaf6c132b22bbb8d33cb84098c5d5d6e5
Nguyễn Đức Thuận <kenzilian13@gmail.com>
2017-02-14 15:59:39 +0700

    Baseline
    Basic Layout and setting for HNF webapplication

---
+++ HNFCRM_Chat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HNFCRM_Chat.Startup))]
+namespace HNFCRM_Chat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
