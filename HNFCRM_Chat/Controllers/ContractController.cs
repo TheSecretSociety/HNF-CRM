e3eae2fa6bddd0fc02c7678e9225edd17d10dccf
anhminh7799 <anhminh7799@gmail.com>
2017-03-03 19:41:51 +0700

    Search Customer function is updated

---
+++ HNFCRM_Chat/Controllers/ContractController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HNFCRM_Chat.Controllers
+{
+    public class ContractController : Controller
+    {
+        // GET: Contract
+        public ActionResult Contract()
+        {
+            return View();
+        }
+    }
+}
