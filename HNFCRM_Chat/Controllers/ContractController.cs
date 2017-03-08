8ae1add4a64de17e535599a2ed62e829d662019c
anhminh7799 <anhminh7799@gmail.com>
2017-03-09 00:02:07 +0700

    Update Layout Index

--- HNFCRM_Chat/Controllers/ContractController.cs
+++ HNFCRM_Chat/Controllers/ContractController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HNFCRM_Chat.Models;
 
 namespace HNFCRM_Chat.Controllers
 {
     public class ContractController : Controller
     {
-        // GET: Contract
-        public ActionResult Contract()
+        CP_CRMEntities entities = new CP_CRMEntities();
+
+        // GET: Contract by Customer ID
+        public ActionResult Contract(int id)
         {
-            return View();
+            CONTRACT contract = entities.CONTRACTs.Where(x => x.ID_Customer == id).SingleOrDefault();
+            return View(contract);
+        }
+
+        //Add new Contract
+        public ActionResult AddContract(int id, FormCollection frm)
+        {
+            CONTRACT contract = new CONTRACT();
+
+            return RedirectToAction("Contract");
         }
     }
 }
---
+++ HNFCRM_Chat/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HNFCRM_Chat.Models
+{
+    public class CustomerModel
+    {
+        public CUSTOMER Customer;
+        public REQUIREPRODUCT RequireProduct;
+        public STAFF Staff;
+    }
+}
