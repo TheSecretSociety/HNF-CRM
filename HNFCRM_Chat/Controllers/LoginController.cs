cc5759d492482dd6e6023851b79b0c7e709def12
Ngo Tan Loc <ngotanloc25011995@gmail.com>
2017-03-29 10:17:19 +0700

    Display Personal Information
    display and edit successfully

---
+++ HNFCRM_Chat/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HNFCRM_Chat.Models;
+using System.Web.Security;
+
+namespace HNFCRM_Chat.Controllers
+{
+    public class LoginController : Controller
+    {
+        CP_CRMEntities entities = new CP_CRMEntities();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(String Email, String Password)
+        {
+            // Add your operation implementation here
+            try
+            {
+                var k = entities.STAFFs.Single(x => x.Email == Email && x.Password == Password);
+                if (k != null)
+                {
+                    //TempData["Staff"]=k.ROLE.Role1;
+                    Session["author"] = new STAFF() { Name = k.ROLE.Role1 };
+                    Session["ID"] = new STAFF() { ID = k.ID };
+                    //FormsAuthentication.SetAuthCookie(k.Email, false);
+                    return RedirectToAction("Staff", "Staff");
+                }
+            }
+            catch (Exception e)
+            {
+                TempData["notice"] = "Email hoặc Password nhập không đúng!";
+                return RedirectToAction("Login", "Login");
+                // return Content("<script language='javascript' type='text/javascript'>alert('Thanks for Feedback!');</script>");
+            }
+            return View();
+        }
+
+        //LogOut
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
