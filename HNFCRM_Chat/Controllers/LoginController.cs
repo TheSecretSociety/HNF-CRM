89cdcff63c196bef8589bcf65328a72808462524
Ngo Tan Loc <ngotanloc25011995@gmail.com>
2017-03-31 10:29:51 +0700

    Update something

--- HNFCRM_Chat/Controllers/LoginController.cs
+++ HNFCRM_Chat/Controllers/LoginController.cs
@@ -26,10 +26,11 @@ namespace HNFCRM_Chat.Controllers
                 if (k != null)
                 {
                     //TempData["Staff"]=k.ROLE.Role1;
-                    Session["author"] = new STAFF() { Name = k.ROLE.Role1 };
+                    Session["author"] = new STAFF() { Name = k.Name };
                     Session["ID"] = new STAFF() { ID = k.ID };
+                    Session["Role"] = new STAFF() { ID_Role = k.ROLE.ID };
                     //FormsAuthentication.SetAuthCookie(k.Email, false);
-                    return RedirectToAction("Staff", "Staff");
+                    return RedirectToAction("Index", "Home");
                 }
             }
             catch (Exception e)
