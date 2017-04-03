a5943bd04714275b6b3bc6690cf1057f9f41a0b3
Ngo Tan Loc <ngotanloc25011995@gmail.com>
2017-04-03 14:49:13 +0700

    Update Login

--- HNFCRM_Chat/Controllers/LoginController.cs
+++ HNFCRM_Chat/Controllers/LoginController.cs
@@ -25,18 +25,25 @@ namespace HNFCRM_Chat.Controllers
                 var k = entities.STAFFs.Single(x => x.Email == Email && x.Password == Password);
                 if (k != null)
                 {
+                    //TempData["Staff"]=k.ROLE.Role1;
                     Session["author"] = new STAFF() { Name = k.Name };
                     Session["ID"] = new STAFF() { ID = k.ID };
-                    Session["Role"] = new STAFF() { ID_Role = k.ROLE.ID };
-                    //FormsAuthentication.SetAuthCookie(k.Email, false);
-                    return RedirectToAction("Index", "Home");
+                    Session["Role"] = new STAFF() {ID_Role=k.ID_Role};
+                    var a = Session["Role"] as STAFF;
+                    if(a.ID_Role==1 || a.ID_Role == 2 || a.ID_Role == 3)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    if (a.ID_Role == 4)
+                    {
+                        return RedirectToAction("ProductionLine", "ProductionLine");
+                    }
                 }
             }
             catch (Exception e)
             {
                 TempData["notice"] = "Email hoặc Password nhập không đúng!";
                 return RedirectToAction("Login", "Login");
-                // return Content("<script language='javascript' type='text/javascript'>alert('Thanks for Feedback!');</script>");
             }
             return View();
         }
